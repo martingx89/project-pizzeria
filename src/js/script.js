7e7d732839b9c644ce7d670e5312096491d01b64
Marcin Adamiak <marcin_89@outlook.com>
2023-04-25 21:08:21 +0200

    Add Cart class

--- src/js/script.js
+++ src/js/script.js
@@ -306,6 +306,26 @@
     }
   }
 
+  class Cart {
+    constructor(element) {
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+
+      console.log('New Cart', thisCart);
+    }
+
+    getElements(element) {
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+    }
+  }
+
   const app = {
     initMenu: function () {
       const thisApp = this;
